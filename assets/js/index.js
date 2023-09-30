9054bac3adb77a6323482eb83126e8621f1d32c1
Viktoriia <pecourses>
2023-09-30 18:37:32 +0300

    Add weather example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,46 +1,40 @@
 'use strict';
 
-// Promise
-
-// cb виконається через 1000мс
-// setInterval(cb, 1000);
-
-// cb виконається, коли буде подія click
-// button.addEventListener('click', cb)
-
-// Колбек, вказаний в then виконається тоді,
-// коли проміс state стане fullfilled
-// Колбек, вказаний в catch виконається тоді,
-// коли проміс state стане rejected
-
 const weatherUrl =
   'https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto';
 
 fetch(weatherUrl)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => generateWeather(data))
   .catch(err => console.log('error: ', err));
 
-// Second then form:
-// fetch(weatherUrl)
-//   .then(response => response.json())
-//   .then(
-//     data => console.log(data),
-//     err => console.log('error: ', err)
-//   );
+// Example: відобразити на сторінці поточну температуру з одиницею виміру
+// відобразити темп. від'ємну синім кольором, 0 -чорним
+//                   додатню до 40 - зеленим, >=40 - червоним
 
-// Promise state (pending, fullfilled, rejected)
-//         result          payload     error
+// Task: відобразити швидкість вітру з одиницею виміру
+function generateWeather({
+  current_weather: { temperature, windspeed },
+  current_weather_units: { temperature: tempUnit, windspeed: windUnit },
+}) {
+  const currentTemperatureEl = document.createElement('div');
+  currentTemperatureEl.textContent = `${temperature} ${tempUnit}`;
+  currentTemperatureEl.style.color = calcTemperatureColor(temperature);
 
-console.log('next line');
+  const currentWindspeed = document.createElement('div');
+  currentWindspeed.textContent = `${windspeed} ${windUnit}`;
 
-// callback hell:
-// setTimeout(() => {
-//   setTimeout(() => {
-//     setTimeout(() => {
-//       setTimeout(() => {
-//         setTimeout(() => {}, 2000);
-//       }, 2000);
-//     }, 2000);
-//   }, 2000);
-// }, 1000);
+  document.body.append(currentTemperatureEl, currentWindspeed);
+}
+
+function calcTemperatureColor(temperature) {
+  if (temperature < 0) {
+    return 'blue';
+  } else if (temperature === 0) {
+    return 'black';
+  } else if (temperature > 0 && temperature < 40) {
+    return 'green';
+  } else {
+    return 'red';
+  }
+}
