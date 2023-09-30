e367036a42d04d116cef7f090845455fa2a52a6c
Viktoriia <pecourses>
2023-09-30 19:20:43 +0300

    Refactor weather

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,39 @@
 'use strict';
 
-const weatherUrl =
-  'https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto';
-
 let isCelsiiDegree = true;
 
 const tempUnitBtn = document.getElementById('tempUnitBtn');
-tempUnitBtn.textContent = `Switch to ${isCelsiiDegree ? 'F' : 'C'}`;
-
 tempUnitBtn.onclick = switchTemperatureUnit;
-
 function switchTemperatureUnit() {
-  // поміняти значення прапорця на протилежне
   isCelsiiDegree = !isCelsiiDegree;
-  // поміняти напис на кнопці
+  updateData();
+}
+
+updateData();
+
+function updateData() {
   tempUnitBtn.textContent = `Switch to ${isCelsiiDegree ? 'F' : 'C'}`;
-  // завантажити дані з температурою в нових одиницях
-  fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto${
-      isCelsiiDegree ? '' : '&temperature_unit=fahrenheit'
-    }`
-  )
+
+  const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto${
+    isCelsiiDegree ? '' : '&temperature_unit=fahrenheit'
+  }`;
+
+  fetch(weatherUrl)
     .then(response => response.json())
     .then(data => generateWeather(data))
     .catch(err => console.log('error: ', err));
 }
 
-fetch(weatherUrl)
-  .then(response => response.json())
-  .then(data => generateWeather(data))
-  .catch(err => console.log('error: ', err));
-
 function generateWeather({
   current_weather: { temperature, windspeed },
   current_weather_units: { temperature: tempUnit, windspeed: windUnit },
 }) {
-  const currentTemperatureEl = document.createElement('div');
+  const currentTemperatureEl = document.querySelector('.temp');
   currentTemperatureEl.textContent = `${temperature} ${tempUnit}`;
   currentTemperatureEl.style.color = calcTemperatureColor(temperature);
 
-  const currentWindspeed = document.createElement('div');
+  const currentWindspeed = document.querySelector('.wind');
   currentWindspeed.textContent = `${windspeed} ${windUnit}`;
-
-  document.body.append(currentTemperatureEl, currentWindspeed);
 }
 
 function calcTemperatureColor(temperature) {
