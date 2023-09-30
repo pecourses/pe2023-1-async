9bc23b2f591ddbf02e095e816a9c132750818e3c
Viktoriia <pecourses>
2023-09-30 16:42:09 +0300

    Add json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,25 @@
 'use strict';
 
-alert('qwerty'); // блокування - погано!
+// JSON
 
-console.log('before timeoutv');
+const obj = {
+  number: 1,
+  bigint: 10n,
+  string: 'string',
+  array: [1, 2, false],
+  boolean: true,
+  null: null,
+  plainObject: { prop: 10 },
+  // метод не => JSON
+  getNumber() {
+    return this.number;
+  },
+  // undefined => JSON
+  undefined: undefined,
+};
 
-setTimeout(() => {
-  console.log('in timeout'); // => webApi
-}, 1000);
+// obj => JSON серіалізація
+const objJson = JSON.stringify(obj);
 
-console.log('after timeout');
-
-// call stack
-function f1() {
-  console.trace();
-  console.log('end');
-}
-function f() {
-  console.trace();
-  f1();
-}
-
-f();
+// JSON => obj десеріалізація
+const parseObj = JSON.parse(objJson);
